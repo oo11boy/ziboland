8512109994fc522cb086c2b22a57b0fffefbdf9e
oo11boy <editkhone@gmail.com>
2025-09-28 13:39:15 +0330

    update db

--- src/lib/db.tsx
+++ src/lib/db.tsx
@@ -3,8 +3,8 @@ import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '',
+  user: process.env.MYSQL_USER || 'ziboland',
+  password: process.env.MYSQL_PASSWORD || 'Ra13781379',
   database: process.env.MYSQL_DATABASE || 'ziboland',
   waitForConnections: true,
   connectionLimit: 10,
